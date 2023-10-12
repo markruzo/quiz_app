0cd1844325ebaab3af505d1d9ca231aad92482cb
Mario Krutzikowsky <mario.krutzikowsky@gmail.com>
2023-10-12 14:39:36 +0200

    Quiz App Overflow

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ A simple Quiz App ..
 Text
 
 <div>
-  <img src="Image_1" alt="Image 1" width="400" />
+  <img src="assets/screens/start_screen.png" alt="Start Screen" width="400" />
   <img src="Image_2" alt="Image 2" width="400" />
 </div>
-# quiz_app
+
---
+++ lib/result_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key, required this.chosenAnswers});
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answers': chosenAnswers[i]
+      });
+    }
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numOfTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+    return SizedBox(
+        width: double.infinity,
+        child: Container(
+          margin: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                  'You answerd $numCorrectQuestions out of $numOfTotalQuestions questions correctly!'),
+              QuestionsSummary(getSummaryData()),
+              TextButton(onPressed: () {}, child: const Text('Restart Quiz'))
+            ],
+          ),
+        ));
+  }
+}
